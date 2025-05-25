9e4d2069f287a14eb016bea7c4f3647c4104500c
Deepak-Panaganti <panagantideepak11@gmail.com>
2025-05-25 19:43:27 +0530

    Delete backactionRouters.js

--- backactionRouters.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAuctions, createAuction, placeBid } = require('../controllers/auctionController');
-
-router.get('/', getAuctions);
-router.post('/', createAuction);
-router.post('/:id/bid', placeBid);
-
-module.exports = router;
